afb6ba036e6777c7a9d15880a6d1db6ee04db9ac
Victomas97 <victomas@outlook.es>
2008-07-03 22:18:27 +0200

    Date Commit

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const { exec } = require("child_process");
 
-exec("git commit -am 'Step Testing'", (error, stdout, stderr) => {
+
+
+exec("git add . & git commit --allow-empty --date 2008-07-03 -m 'Date Commit'", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
     }
     if (stderr) {
         console.log(`stderr: ${stderr}`);
+        exec("git push", (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+        });
+
         return;
     }
     console.log(`stdout: ${stdout}`);
 });
+
+// fetch("https://github.com/users/Victomas97/contributions").then((res) => {
+//     console.log(res.json());
+// });
