ac562e55fe4bb4c8642041b3e05983bc3f279c1f
Victomas97 <victomas@outlook.es>
2023-07-05 22:30:30 +0200

    Starting Project

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 const { exec } = require("child_process");
 
+const initDate = new Date('2018-01-01');
+const endDate = new Date('2018-12-31');
 
+// Crear una matriz vacía para almacenar las fechas
+const dateMatrix = [];
 
-exec("git add . & git commit --allow-empty --date 2008-07-03 -m 'Date Commit'", (error, stdout, stderr) => {
+// Recorrer el rango de fechas y agregar cada fecha a la matriz
+const currentDate = initDate;
+while (currentDate <= endDate) {
+    dateMatrix.push(`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`);
+    currentDate.setDate(currentDate.getDate() + 1);
+}
+
+dateMatrix.forEach((date) => {
+    exec(`git commit --allow-empty --date ${date} -m 'Date Commit'`, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+});
+
+exec("git push", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
     }
     if (stderr) {
         console.log(`stderr: ${stderr}`);
-        exec("git push", (error, stdout, stderr) => {
-            if (error) {
-                console.log(`error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-
-                return;
-            }
-            console.log(`stdout: ${stdout}`);
-        });
 
         return;
     }
